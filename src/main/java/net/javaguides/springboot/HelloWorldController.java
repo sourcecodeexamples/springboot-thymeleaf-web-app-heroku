12f339cb7195128d39fb30d8ad6fe7f97659e71b
RameshMF <ramesh24fadatare@gmail.com>
2020-11-23 16:09:34 +0530

    spring boot thymelaf example

---
+++ src/main/java/net/javaguides/springboot/HelloWorldController.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello World");
+		return "helloworld";
+	}
+}
